b4b2f2fb360e18284e69b1ccdb8bdd041ec17d96
Georges-Adrien-Ramos <georgesadrien.ramos@unchk.edu.sn>
2025-10-15 17:28:53 +0000

    Add files via upload

---
+++ motivationlife-bibliothèque.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bibliothèque</title>
+    <link rel="stylesheet" href="motivationlife-bibliothèque.css">
+</head>
+<body>
+    
+
+ 
+<!-- SECTION MENU DE NAVIGATION -->
+  <!-- HEADER -->
+  <header>
+    <div class="menu-icon" id="menuToggle">☰</div>
+      <!-- MENU HORIZONTAL CACHÉ -->
+  <nav id="menuBar">
+    <ul>
+      <li><a href="motivationlife-accueil.php">ACCUEIL</a></li>
+      <li><a href="motivationlife-à-propos.php">A PROPOS</a></li>
+      <li><a href="#">BLOGS</a></li>
+      <li><a href="motivationlife-témoignage.php">TEMOIGNAGES</a></li>
+      
+      
+      
+    </ul>
+  </nav>
+  <div class="menu-right">
+   <a href="motivationlife-contact.php"><img src="images/icon-google-contacts.png" alt="lien contact"></a>
+   <a class="bibliothèque" href="motivationlife-bibliothèque.php">bibliothèque</a>
+  <img class="image-bibliothèque" src="images/icon-bibliotheque-publique.png" alt="lien bibliothèque">
+  </div>
+  </header>
+
+<!-- image arrière-plan -->
+  <div class="image-background">
+   <img src="images/SOLEIL.jpg" alt="image-background">
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="Presentation-bibliothèque">
+    <h1>Découvez <br> la bibilothèque <br> motivationlife !!!</h1>
+    <P>Livre PDF avec des Thèmes frappant </P>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <main class="container">
+        <section class="filtres-rapides">
+            <div class="search-bar">
+                <ion-icon name="search-outline"></ion-icon>
+                <input  class="texte-searh-bar" type="text" placeholder="Chercher un titre, un thème...">
+            </div>
+            <div class="categories-filtres">
+                <button class="filter-btn active" data-filter="all">Tous les Thèmes</button>
+                <button class="filter-btn" data-filter="économie">économie</button>
+                <button class="filter-btn" data-filter="psychologie">psychologie</button>
+                <button class="filter-btn" data-filter="developpement-personnel">developpement personnel</button>
+               
+            </div>
+             </section>
+
+        
+        <div class="Themes-econnomique"> 
+                <h2>toute la gamme motivationlife</h2>
+        </div>  
+
+        <section class="ressources-grille">
+            
+           
+            <div class="ressource-card" data-category="économie">
+               
+                <img src="images/père riche, père pauvre.jpg" alt="Couverture père riche, père pauvre">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>Père riche, père pauvre</h4>
+                    <p>Robert T. KIYOSAKI</p>
+                    <div class="étoile">
+                         <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur économie/Père riche, père pauvre (Robert T. Kiyosaki) (Z-Library).pdf" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+            
+            <div class="ressource-card" data-category="économie">
+                <img src="images/l'homme  le plus riche de babylone.jpg" alt="Couverture l'homme le plus riche de babylone ">
+                <div class="card-content">
+                    <span class="badge tendance">Tendance</span>
+                    <h4>L'homme le plus riche de Babylone </h4>
+                    <p>George Samuel CLASON</p>
+                    <div class="étoile">
+                         <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur économie/Lhomme le plus riche de Babylone (George Samuel Clason) (Z-Library).pdf" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+
+            <div class="ressource-card" data-category="économie">
+                <img src="images/tout le monde n'a pas eu la change de rater ces études.jpg" alt="Couverture tout le monde n'a pas eu la change de rater ces études">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>Tout le monde n’a pas eu la chance de rater ses études</h4>
+                    <p>Olivier ROLAND</p>
+                    <div class="étoile">
+                        <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur économie/Tout le monde n’a pas eu la chance de rater ses études (Olivier Roland) (Z-Library).pdf" class="btn-card"download>télécharge le livre ici</a>
+                </div>
+            </div>
+            <div class="ressource-card" data-category="économie">
+                <img src="images/Le Personal MBA.jpg" alt="Couverture Le Personal MBA">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>Le Personal MBA</h4>
+                    <p>KAUFMAN Josh</p>
+                    <div class="étoile">
+                        <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur économie/Le Personal MBA (KAUFMAN Josh) (Z-Library).pdf" class="btn-card"download>télécharge le livre ici</a>
+                </div>
+            </div>
+            
+            <div class="ressource-card" data-category="psychologie">
+                <img src="images/Les vertus de l'échec.jpg" alt="Couverture Les vertus de l'échec ">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>Les vertus de l'échec</h4>
+                    <p>Charles PEPIN</p>
+                    <div class="étoile">
+                    <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur la psychologie/Les vertus de léchec (Charles Pépin) (Z-Library).pdf" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+
+            <div class="ressource-card" data-category="psychologie">
+                <img src="images/De l’Intention à la Réalisation, Secrets d'une Motivation à Toute Epreuve.jpg" alt="Couverture De l’Intention à la Réalisation, Secrets d'une Motivation à Toute Epreuve">
+                <div class="card-content">
+                    <span class="badge tendance">Tendance</span>
+                    <h4>De l’Intention à la Réalisation, Secrets d'une Motivation à Toute Epreuve</h4>
+                    <p>Yves-Alexandre TALMANN</p>
+                    <div class="étoile">
+                    <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur la psychologie/De l’Intention à la Réalisation, Secrets dune Motivation à Toute Epreuve (Yves-Alexandre Thalmann) (Z-Library).pdf" class="btn-card" download >télécharge le livre ici</a>
+                </div>
+            </div>
+
+            <div class="ressource-card" data-category="psychologie">
+                <img src="images/Psychologie de la motivation.jpg" alt="Couverture Psychologie de la motivation">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>Psychologie de la motivation</h4>
+                    <p>Paul DIEL</p>
+                    <div class="étoile">
+                    <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur la psychologie/Psychologie de la motivation (Paul Diel) (Z-Library).pdf" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+            
+            <div class="ressource-card" data-category="psychologie">
+                <img src="images/L'art de la méditation.jpg" alt="Couverture L'art de la méditation">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>L'art de la méditation</h4>
+                    <p> Matthieu RICARD</p>
+                    <div class="étoile">
+                    <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur la psychologie/Lart de la méditation (Matthieu Ricard) (Z-Library).pdf" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+            
+            <div class="ressource-card" data-category="developpement-personnel">
+                <img src="images/Comment partir à la conquête de soi-même, devenir un maître de la communication, et développer une confiance en ....jpg" alt="Couverture Comment partir à la conquête de soi-même, devenir un maître de la communication, et développer une confiance en ...">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>Comment partir à la conquête de soi-même, devenir un maître de la communication, et développer une confiance en ...</h4>
+                    <p>Robert MERCIER</p>
+                    <div class="étoile">
+                        <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur le développement personnel/DÉVELOPPEMENT PERSONNEL 3 livres en 1 - Comment partir à la conquête de soi-même, devenir un maître de la communication, et… (Robert Mercier) (Z-Library).pdf" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+            
+            <div class="ressource-card" data-category="developpement-personnel">
+                <img src="images/Le Maître en soi, Les enseignements du Maître Saint-Germain.jpg" alt="Couverture Le Maître en soi, Les enseignements du Maître Saint-Germain">
+                <div class="card-content">
+                    <span class="badge tendance">Tendance</span>
+                    <h4>Le Maître en soi : Les enseignements du Maître Saint-Germain</h4>
+                    <p>Lessard PIERRE, Gauthier FRANCE</p>
+                    <div class="étoile">
+                         <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="livre de motivation sur le développement personnel/Le Maître en soi  Les enseignements du Maître Saint-Germain (Lessard Pierre, Gauthier France) (Z-Library).pdf" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+
+            <div class="ressource-card" data-category="developpement-personnel">
+                <img src="images/Stratégie, Les 33 lois de la guerre.jpg" alt="Couverture Stratégie : Les 33 lois de la guerre">
+                <div class="card-content">
+                    <span class="badge classique">Classique</span>
+                    <h4>Stratégie : Les 33 lois de la guerre</h4>
+                    <p>Robert GREENE</p>
+                    <div class="étoile">
+                        <li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li><ion-icon name="star"></ion-icon></li><li> <ion-icon name="star-half"></ion-icon></li>
+                    </div>
+                    <a href="#" class="btn-card" download>télécharge le livre ici</a>
+                </div>
+            </div>
+
+            
+        </section>
+    </main>
+
+
+
+
+
+
+    
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+
+    <script src="motivation-bibliothèque.js"></script>
+</body>
+</html>
