2b3a1fc4b3f89debf67fa0a27dedee11b113f48a
Georges-Adrien-Ramos <georgesadrien.ramos@unchk.edu.sn>
2025-10-15 17:27:44 +0000

    Add files via upload

---
+++ motivationlife-à-propos.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>à propos</title>
+    <link rel="stylesheet" href="motivationlife-à-propos.css">
+</head>
+<body>
+
+
+
+    <!-- SECTION MENU DE NAVIGATION -->
+  <!-- HEADER -->
+  <header>
+    <div class="menu-icon" id="menuToggle">☰</div>
+      <!-- MENU HORIZONTAL CACHÉ -->
+  <nav id="menuBar">
+    <ul>
+        <li><a href="motivationlife-accueil.php">ACCUEIL</a></li>
+      <li><a href="motivationlife-à-propos.php">A PROPOS</a></li>
+      <li><a href="#">BLOGS</a></li>
+      <li><a href="motivationlife-témoignage.php">TEMOIGNAGES</a></li>
+      
+      
+      
+    </ul>
+  </nav>
+   <a href="motivationlife-contact.php"><img src="icon-google-contacts.png" alt="lien contact"></a>
+   <a class="bibliothèque" href="motivationlife-bibliothèque.php">bibliothèque</a>
+  <img class="image-bibliothèque" src="images/icon-bibliotheque-publique.png" alt="lien bibliothèque">
+  </header>
+
+
+
+
+
+  <!-- image en arrière-plan -->
+  <div class="image-background">
+    <img src="SOLEIL.jpg" alt="">
+  </div>
+
+
+  
+
+
+
+
+
+
+
+
+  <div data-scroll-container class="content-smooth">
+  <div class="texte-a-propos">
+ <!-- TEXTE A PROPOS -->
+  <h1>A PROPOS</h1>
+  <h2>Le feu naît de la fumée. Toi aussi, allume ta flamme!!!</h2> 
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing <br> Earum aliquid incidunt perspiciatis natus harum! Numquam, <br> repellat commodi. Ipsa itaque odit esse, recusandae <br> quibusdam obcaecati at officiis in nobis <br> aliquid explicabo?</p>
+  </div>
+
+  <section>
+    
+    <div class="smooth-scroll-content">
+      <h2>notre mission</h2>
+      <p data-scroll data-scroll-speed="2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium aspernatur deleniti quae maxime eveniet vitae minus? Sapiente ut deleniti dolor numquam nemo, quis mollitia dolorum adipisci a placeat dolorem non.</p>
+    </div>
+    <div class="smooth-scroll-img">
+      <img src="images/NEW.jpg" alt="">
+    </div>
+  </section>
+   <div class="citation-notre-mission">
+    <pre>
+    " Ce que vous obtenez en atteignant vos objectifs n’est pas aussi important 
+      que ce que vous devenez en les atteignant. Le vrai succès ne réside  pas 
+      dans la récompense, mais dans la transformation intérieure, dans la force,
+      la discipline et la clarté que vous gagnez en chemin. "  — Zig Ziglar
+
+
+    </pre>
+    </div>
+  <section>
+    <div class="smooth-scroll-img">
+      <img src="images/NEW.jpg" alt="">
+    </div>
+    <div class="smooth-scroll-content">
+      <h2>nos cibles</h2>
+      <p data-scroll data-scroll-speed="2">  <SPan>Étudiants et jeunes actifs </SPan> en quête de repères, de motivation et de développement personnel. <br> <span>Personnes en transition </span> (reconversion, remise en question, période difficile) qui cherchent à se reconstruire ou à se réinventer. <br> <span> Créateurs et porteurs de projets </span> qui ont besoin d’inspiration, de clarté et d’un environnement stimulant. <br> <span> Curieux du bien-être mental et émotionnel </span>, intéressés par des contenus motivants, des outils pratiques et des témoignages inspirants. <br> <span> Utilisateurs introvertis ou discrets</span>, qui préfère un espace digital bienveillant pour s’exprimer et évoluer à leur rythme.</p>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+     <!-- SECTION NOUVEAUX DANS LA bibliothèque -->
+  <section class="teams">
+    <div class="teams-header">
+      <h2  >TEAMS</h2>
+    </div>
+
+    <!-- SLIDER -->
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Développement personnel">
+            <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Motivation & Leadership">
+              <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Succès & Carrière">
+              <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Santé mentale & Bien-être">
+              <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Santé mentale & Bien-être">
+              <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Santé mentale & Bien-être">
+              <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+        
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Santé mentale & Bien-être">
+              <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="teams-card">
+            <img src="images/team 1.jpg" alt="Spiritualité & Inspiration">
+           <h3>Georges Adrien Ramos</h3>
+            <h4>Responsable de la teams</h4>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Pagination -->
+      <div class="swiper-pagination"></div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="adhesion-whathsapp">
+  <div>
+  <h2>adhèsion groupe WhatsApp</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Delectus excepturi eos dolorem culpa esse. <br> Aperiam provident saepe impedit minus expedita.</p>
+  <a href="#"><h3>cliquez ici pour accéder au groupe WhatsApp</h3></a>
+  </div>
+  <div class="image-adhesion-whathsapp">
+    <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="image-adhesion-whathsapp">
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+  <div class="newsletter">
+    <h1>motivationlife newsletter</h1>
+    <h2>Abonnez-vous à notre Newsletter</h2>
+    <form action="#" method="post">
+      <input type="email" name="email" placeholder=" Veuillez entrez votre adresse e-mail" required>
+      <button type="submit">Soumettre</button>
+    </form>
+</div>
+
+
+
+
+
+<div class="footer ">
+  <div class="content-principal-footer">
+  <h1>motivationlife</h1>
+  <div class="nosPages">
+    <a href="#"><img src="images/icon tiktok.png" alt="logo-tiktok"></a>
+    <a href="#"><img src="images/icon facebook.png" alt="logo-facebook"></a>
+    <a href="#"><img src="images/icon instagram.png" alt="logo-instagram"></a>
+    <a href="#"><img src="images/icon twitter.png" alt="logo-twiter"></a>
+    <a href="#"><img src="images/icon whatsapp.png" alt="logo-WhatsApp"></a>
+  </div>
+  </div>
+  <h4 class="copyright">copyright@ 2025</h4>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="locomotive-scroll.min.js"></script>
+<script>
+    const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+</script>
+
+<!-- Swiper JS -->
+  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+      <!-- Script JS -->
+  <script src="motivationlife-à-propos.js"></script>
+</body>
+</html>
