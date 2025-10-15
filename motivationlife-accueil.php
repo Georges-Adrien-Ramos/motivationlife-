207542a9f67f222134957d076f1314c0be7352cb
Georges-Adrien-Ramos <georgesadrien.ramos@unchk.edu.sn>
2025-10-15 17:25:52 +0000

    Add files via upload

---
+++ motivationlife-accueil.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Acueil</title>
+  <!-- lien icone home -->
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=home" />
+
+  <!-- Swiper CSS -->
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+
+  <!-- Feuille CSS -->
+  <link rel="stylesheet" href="motivationlife-accueil.css">
+</head>
+<body>
+
+
+
+<!-- SECTION MENU DE NAVIGATION -->
+  <!-- HEADER -->
+  <header>
+    <div class="menu-icon" id="menuToggle">☰</div>
+      <!-- MENU HORIZONTAL CACHÉ -->
+  <nav id="menuBar">
+    <ul>
+      <li><a href="motivationlife-accueil.php">ACCUEIL</a></li>
+      <li><a href="motivationlife-à-propos.php">A PROPOS</a></li>
+      <li><a href="#">BLOGS</a></li>
+      <li><a href="motivationlife-témoignage.php">TEMOIGNAGES</a></li>
+      
+      
+      
+    </ul>
+  </nav>
+   <a href="motivationlife-contact.php"><img src="images/icon-google-contacts.png" alt="lien contact"></a>
+   <a class="bibliothèque" href="motivationlife-bibliothèque.php">bibliothèque</a>
+  <a class="image-bibliothèque" href="motivationlife-bibliothèque.php"><img  src="images/icon-bibliotheque-publique.png" alt="lien bibliothèque"></a>
+  </header>
+
+
+
+
+
+<!-- Vidéo en arrière-plan -->
+  <div class="video-background">
+    <video autoplay muted loop>
+      <source src="images/WhatsApp Vidéo 2025-09-09 à 18.38.59_ec2cd1e0.mp4" type="video/mp4">
+    </video>
+  </div>
+
+
+
+
+
+
+
+  <!-- logo-slogan -->
+
+  <!-- Contenu -->
+  <div class="content">
+    <h1 class="nomDuSite">motivationlife</h1>
+    <h2 class="slogan">Le feu naît de la fumée. Toi aussi, allume ta flamme!!!</h2>
+    <p class="descriptionDuSite"> Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Ipsum ut blanditiis omnis facilis quis soluta quas eaque nesciunt, deserunt veniam quaerat  <br/> rem consectetur cum dignissimos deleniti. Voluptatibus consectetur magnam mollitia sequi qui?</p>
+  </div>
+  <div class="citationDuJour"><pre>
+      "Citation du jour:
+      le Succès appartient à
+      qui se lèvent tôt et
+      à ceux qui ne reculent 
+            jamais."
+    </pre></div>
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- SECTION CATÉGORIES -->
+  <section class="categories">
+    <div class="categories-header">
+      <h2  >CATÉGORIES</h2>
+      <div class="nav-buttons">
+        <div class="swiper-button-prev">  </div>
+        <div class="swiper-button-next">  </div>
+      </div>
+    </div>
+
+    <!-- SLIDER -->
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+
+        <div class="swiper-slide">
+          <div class="category-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Développement personnel">
+            <h3>Développement personnel</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="category-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Motivation & Leadership">
+            <h3>Motivation & Leadership</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="category-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Succès & Carrière">
+            <h3>Succès & Carrière</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="category-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Santé mentale & Bien-être">
+            <h3>Santé mentale & Bien-être</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="category-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Spiritualité & Inspiration">
+            <h3>Spiritualité & Inspiration</h3>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Pagination -->
+      <div class="swiper-pagination"></div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- SECTION NOUVEAUX ARTICLES -->
+  <section class="articles">
+    <div class="articles-header">
+      <h2  >NOUVEAUX ARTICLES</h2>
+      <div class="nav-buttons">
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+      </div>
+    </div>
+
+    <!-- SLIDER -->
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+
+        <div class="swiper-slide">
+          <div class="articles-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Développement personnel">
+            <h3>Développement personnel</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="articles-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Motivation & Leadership">
+            <h3>Motivation & Leadership</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="articles-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Succès & Carrière">
+            <h3>Succès & Carrière</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="articles-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Santé mentale & Bien-être">
+            <h3>Santé mentale & Bien-être</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="articles-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Spiritualité & Inspiration">
+            <h3>Spiritualité & Inspiration</h3>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Pagination -->
+      <div class="swiper-pagination"></div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+    <!-- SECTION NOUVEAUX DANS LA bibliothèque -->
+  <section class="bibliothèque">
+    <div class="bibliothèque-header">
+      <h2  >NOUVEAUtés DANS LA bibliothèque</h2>
+    </div>
+
+    <!-- SLIDER -->
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+
+        <div class="swiper-slide">
+          <div class="bibliothèque-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Développement personnel">
+            <h3>Développement personnel</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="bibliothèque-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Motivation & Leadership">
+            <h3>Motivation & Leadership</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="bibliothèque-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Succès & Carrière">
+            <h3>Succès & Carrière</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="bibliothèque-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Santé mentale & Bien-être">
+            <h3>Santé mentale & Bien-être</h3>
+          </div>
+        </div>
+
+        <div class="swiper-slide">
+          <div class="bibliothèque-card">
+            <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="Spiritualité & Inspiration">
+            <h3>Spiritualité & Inspiration</h3>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Pagination -->
+      <div class="swiper-pagination"></div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="adhesion-whathsapp">
+  <div>
+  <h2>adhèsion groupe WhatsApp</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Delectus excepturi eos dolorem culpa esse. <br> Aperiam provident saepe impedit minus expedita.</p>
+  <a href="#"><h3>cliquez ici pour accéder au groupe WhatsApp</h3></a>
+  </div>
+  <div class="image-adhesion-whathsapp">
+    <img src="images/WhatsApp Image 2025-09-09 à 18.38.43_ac90420b.jpg" alt="image-adhesion-whathsapp">
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="newsletter">
+    <h1>motivationlife newsletter</h1>
+    <h2>Abonnez-vous à notre Newsletter</h2>
+    <form action="#" method="post">
+      <input type="email" name="email" placeholder=" Veuillez entrez votre adresse e-mail" required>
+      <button type="submit">Soumettre</button>
+    </form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="footer ">
+  <div class="content-principal-footer">
+  <h1>motivationlife</h1>
+  <div class="nosPages">
+    <a href="#"><img src="images/icon tiktok.png" alt="logo-tiktok"></a>
+    <a href="#"><img src="images/icon facebook.png" alt="logo-facebook"></a>
+    <a href="#"><img src="images/icon instagram.png" alt="logo-instagram"></a>
+    <a href="#"><img src="images/icon twitter.png" alt="logo-twiter"></a>
+    <a href="#"><img src="images/icon whatsapp.png" alt="logo-WhatsApp"></a>
+  </div>
+  </div>
+  <h4 class="copyright">copyright@ 2025</h4>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Swiper JS -->
+  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+  <!-- Script JS -->
+  <script src="motivationlife-accueil.js"></script>
+</body>
+</html>
